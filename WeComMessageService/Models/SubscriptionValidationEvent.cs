ef226df132b9b31d58581657c4cbee80bb3070e4
Victor Jiang <victor.jiang@aptean.com>
2023-03-07 16:25:24 +0800

    Add App Configuration Refresher

---
+++ WeComMessageService/Models/SubscriptionValidationEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace WeComMessageService.Models
+{
+    public class SubscriptionValidationEvent
+    {
+        public string id { get; set; }
+        public string topic { get; set; }
+        public string subject { get; set; }
+        public SubscriptionValidationEventData data { get; set; }
+        public string eventType { get; set; }
+        public DateTime eventTime { get; set; }
+        public string metadataVersion { get; set; }
+        public string dataVersion { get; set; }
+    }
+
+    public class SubscriptionValidationEventData
+    {
+        public string validationCode { get; set; }
+        public string validationUrl { get; set; }
+    }
+}
